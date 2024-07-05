587ee1a81e6b90cc3ce4e8d7035d1278e426500c
Will <71314183+BK-ni@users.noreply.github.com>
2024-07-05 16:40:07 +0800

    Add files via upload

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -1,26 +1,44 @@
 from turtle import Turtle
-
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
-        start_x = 0
-        for segment in range(0, 3):
+        for position in STARTING_POSITION:
             new_segment = Turtle("square")
             new_segment.color("white")
             new_segment.penup()
-            new_segment.goto(x=start_x, y=0)
+            new_segment.goto(position)
             self.segments.append(new_segment)
-            start_x -= 20
 
+    # Important technique: for i in range(start, end, step)
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(x=new_x, y=new_y)
-        self.segments[0].fd(20)
-        self.segments[0].right(90)
+        self.segments[0].fd(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.segments[0].setheading(UP)
+    def down(self):
+        if self.head.heading() != UP:
+            self.segments[0].setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.segments[0].setheading(LEFT)
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.segments[0].setheading(RIGHT)
 
 
