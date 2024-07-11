9ae56732f26be3e2cc2d65564106f75491652962
Will Ni <will.ni@cancerfree.io>
2024-07-11 18:09:24 +0800

    0711 update

--- pong-game/main.py
+++ pong-game/main.py
@@ -26,21 +26,21 @@ screen.onkey(r_paddle.down, "Down")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 320:
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 and ball.xcor() :
         ball.bounce_x()
     elif ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
-    else:
-        if ball.distance(r_paddle) > ball.distance(l_paddle):
-            scoreboard.player_b += 1
-            ball.__init__()
-        else:
-            scoreboard.player_a += 1
-            ball.__init__()
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 screen.exitonclick()
--- pong-game/scoreboard.py
+++ pong-game/scoreboard.py
@@ -10,6 +10,18 @@ class ScoreBoard(Turtle):
         self.hideturtle()
         self.color("white")
         self.goto(x=0, y=240)
-        self.player_a = 0
-        self.player_b = 0
-        self.write(f"{self.player_a} : {self.player_b}", move=False, align=ALIGN, font=FONT)
+        self.l_score = 0
+        self.r_score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"{self.l_score} : {self.r_score}", move=False, align=ALIGN, font=FONT)
+
+    def l_point(self):
+        self.l_score += 1
+        self.update_scoreboard()
+
+    def r_point(self):
+        self.r_score += 1
+        self.update_scoreboard()
