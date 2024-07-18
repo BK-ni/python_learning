78e49a3121ba638f05fe424d03049831a84925ea
Will <b0910688@gmail.com>
2024-07-18 22:08:07 +0800

    0718 update

--- day-25/main.py
+++ day-25/main.py
@@ -2,11 +2,18 @@
 #     data = data_file.readlines()
 #     print(data)
 
-import csv
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperature = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperature.append(int(row[1]))
+#
+# print(temperature)
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperature = []
-    for row in data[1:]:
-        temperature.append(row[1])
-        print(temperature)
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
