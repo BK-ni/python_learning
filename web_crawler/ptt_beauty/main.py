19e9698703302a96a934baaed28085bf987005e3
Will <b0910688@gmail.com>
2024-08-12 21:57:39 +0800

    0812 update

--- web_crawler/ptt_beauty/main.py
+++ web_crawler/ptt_beauty/main.py
@@ -1,8 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.ptt.cc/bbs/Beauty/index1.html"
+url = "https://www.ptt.cc/bbs/Beauty/M.1686997472.A.FDA.html"
 headers = {"Cookie": "over18=1"}
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.text, "html.parser")
-print(soup.prettify())
+# print(soup.prettify())
+
+spans = soup.find_all("span", class_="article-meta-value")
+print(spans[2].text)
+
