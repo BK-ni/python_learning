b499b022e4b8e1b44859bec32c4cf365d20073c7
Will Ni <will.ni@cancerfree.io>
2024-08-23 17:07:56 +0800

    0823 update

--- web_crawler/doctor_list/main.py
+++ web_crawler/doctor_list/main.py
@@ -12,11 +12,23 @@ soup = BeautifulSoup(response.text, "html.parser")
 # print(soup.prettify())
 
 a_tags = soup.find_all("a", class_="list-group-item-action d-flex align-items-stretch justify-content-between align-items-center p-2 px-3 top_link")
-for i in range(len(a_tags)):
-    anchor_text = a_tags[i].get_text().strip()
-    key, value = anchor_text.split(maxsplit=1)
-    result_dict[value] = key
-    print(result_dict)
+for a_tag in a_tags:
+    try:
+        anchor_text = a_tag.get_text().strip()
+        key, value = anchor_text.split(maxsplit=1)
+        result_dict[value] = key
+        href = a_tag.get("href")
+        response = requests.get(href)
+        ability = BeautifulSoup(response.text, "html.parser")
+        sixth_tr = ability.find("div", class_="table-responsive ckeditor").find_all("tr")[5]
+        p_text = sixth_tr.find_all('td')[1].find('p')
+        print(p_text)
+        print(href)
+    except ValueError as e:
+        print(f"Error processing text '{anchor_text}': {e}")
+
+    except Exception as e:
+        print(f"Un expected error occurred: {e}")
 print(result_dict)
 
 # 進入到各醫生個人版，將其'專長學科'存成新的{key: value}
